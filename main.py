4ab42938da7b0c0634932f5fc9a0ab716678da8c
JunSpring <wjdwnsqja08@gmail.com>
2021-11-15 16:50:46 +0900

    2nd commit

--- main.py
+++ main.py
@@ -22,27 +22,41 @@ background = pygame.transform.scale(background, (1200, 550))
 class score_board:
     def __init__(self, type):
         self.image = pygame.image.load("data/score_board/score_board.png")
-        self.image_shadow = pygame.image.load("data/score_board/score_board_shadow.png")
+        self.image = pygame.transform.scale(self.image, (300, display_height - 20 * 2))
         self.type = type
 
         if self.type == 'Left':
-            self.x = -self.image.get_width() - 10
-            self.y = 10
+            self.x = 10
+            self.y = (display_height - self.image.get_height()) // 2
         else:
-            self.x = self.image.get_width() + 10
-            self.y = 10
+            self.x = display_width - self.image.get_width() - 10
+            self.y = (display_height - self.image.get_height()) // 2
 
-    def fade_in(self):
-        if self.type == 'Left':
-            pass
-        else:
-            pass
+    # def set(self):
+    #     screen.blit(self.image, (self.x, self.y))
+    #
+    # def fade_in(self):
+    #     if self.type == 'Left':
+    #         pass
+    #     else:
+    #         pass
+    #
+    # def fade_out(self):
+    #     if self.type == 'Left':
+    #         pass
+    #     else:
+    #         pass
 
-    def fade_out(self):
-        if self.type == 'Left':
-            pass
-        else:
-            pass
+
+left_score_board = score_board('Left')
+right_score_board = score_board('Right')
+
+
+# dice ----------------------------------------------------
+class dice:
+    def __init__(self, num):
+        self.num = num
+        self.image = pygame.image.load("data/dice/dice_" + num + '.png')
 
 
 # title ----------------------------------------------------
@@ -61,7 +75,7 @@ tittle_y = display_height // 2 - (tittle.get_height() + press_SPACE.get_height()
 tittle_shadow_x = tittle_x + 6
 tittle_shadow_y = tittle_y + 6
 
-press_SPACE_x = display_width // 2 - press_SPACE.get_width()//2
+press_SPACE_x = display_width // 2 - press_SPACE.get_width() // 2
 press_SPACE_y = tittle_y + tittle.get_height() + 20
 press_SPACE_shadow_x = press_SPACE_x + 4
 press_SPACE_shadow_y = press_SPACE_y + 4
@@ -81,17 +95,19 @@ while running:
         screen.blit(press_SPACE, (press_SPACE_x, press_SPACE_y))
 
     if game_prepare_phase:
-        pass
+        left_score_board.set()
+        right_score_board.set()
 
     for event in pygame.event.get():
+        print(event)
         if event.type == pygame.QUIT:
             running = False
 
         if event.type == KEYDOWN:
             if event.key == K_SPACE:
                 if tittle_phase:
+                    game_prepare_phase = True
                     tittle_phase = False
-                    game_start_phase = True
 
     pygame.display.update()
     mainClock.tick(60)
